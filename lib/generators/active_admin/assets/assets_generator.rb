6394479684804905d1fd2383eae81aeef0c5fad3
Jason Taylor <jason@redant.com.au>
2026-01-12 12:08:05 +1100

    Update assets_generator to install correct css

--- lib/generators/active_admin/assets/assets_generator.rb
+++ lib/generators/active_admin/assets/assets_generator.rb
@@ -5,10 +5,16 @@ module ActiveAdmin
       source_root File.expand_path("templates", __dir__)
 
       def install_assets
-        remove_file "app/assets/stylesheets/active_admin.scss"
         remove_file "app/assets/javascripts/active_admin.js"
-        template "active_admin.css", "app/assets/stylesheets/active_admin.css"
-        template "tailwind.config.js", "tailwind-active_admin.config.js"
+
+        if defined?(Tailwindcss::Engines)
+          template "active_admin.css", "app/assets/tailwind/application.css"
+        else
+          template "active_admin.css", "app/assets/stylesheets/active_admin.css"
+
+          remove_file "app/assets/builds/tailwind/active_admin.css"
+          template "engine.css.erb", "app/assets/builds/tailwind/active_admin.css"
+        end
       end
     end
   end
